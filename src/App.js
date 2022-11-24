09fd5dd82e1e4bfb8fc48e12952eafad86b8223b
khaled nassar <58118235+khalednassar500@users.noreply.github.com>
2022-11-24 07:18:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {  // Functional Component
     <Routes>
         <Route 
           exact
-          path='/'
+          path='/my-reads-app'
           element={
             <div className="App">
               <header className='main-header'>
